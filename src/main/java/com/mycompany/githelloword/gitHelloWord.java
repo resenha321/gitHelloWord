87c0c54d93d3dc60b8286c0bb78a4d031891a32b
Resenha321 <Fernandozribeiro.fr@gmail.com>
2022-03-15 15:47:42 -0300

    Adicionando GitHelloApp com a main() - v1.0

---
+++ src/main/java/com/mycompany/githelloword/gitHelloWord.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.githelloword;
+
+/**
+ *
+ * @author devsys-a
+ */
+public class gitHelloWord {
+    public static void main(String[] args) {
+        
+    }
+}
